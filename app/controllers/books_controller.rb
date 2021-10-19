2bbeb458da9bf55e993f0a1f8a32b8023e1d8cd6
Ikemoto-Yu <cosper79@gmail.com>
2021-10-19 14:32:48 +0900

    テンプレートshowの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,10 +2,7 @@ class BooksController < ApplicationController
   protect_from_forgery except: [:destroy]
   before_action :set_book, only:[:show, :destroy]
   def show
-    respond_to do |format|
-      format.html
-      format.json
-    end
+    render :show
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get "/book/:id" => "books#show"
   delete "/book/:id" => "books#destroy"
+  resources :publishers
+  resource :profile, only: %i{show edit update}
 end
